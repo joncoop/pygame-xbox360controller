5201cde8d8e7259de98fd15c4b495fa2de7139a9
Jon Cooper <joncoop1972@gmail.com>
2015-11-11 20:22:54 -0500

    First commit

---
+++ demo.py
@@ -0,0 +1,195 @@
+import pygame
+from xbox_360_controller import XBox360Controller
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 65, 65)
+GREEN = (75, 225, 25)
+BLUE = (65, 65, 255)
+AMBER = (255, 175, 0)
+GREY = (175, 175, 175)
+
+
+pygame.init()
+
+size = [600, 670]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("X-Box 360 Controller")
+
+FPS = 30
+clock = pygame.time.Clock()
+
+
+
+# make a controller (should this be in loop?)
+controller = XBox360Controller(0)
+
+
+def display_text(screen, text, x, y):
+    my_font = pygame.font.Font(None, 30)
+    output = my_font.render(text, True, WHITE)
+    screen.blit(output, [x, y])
+
+# game loop
+done = False
+
+while not done:
+    # event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done=True
+
+    a_btn = controller.a()
+    b_btn = controller.b()
+    x_btn = controller.x()
+    y_btn = controller.y()
+    back = controller.back()
+    start = controller.start()
+    lt_bump= controller.left_bumper()
+    rt_bump = controller.right_bumper()
+    triggers = controller.triggers()
+    lt_stick = controller.left_stick_axes()
+    rt_stick = controller.right_stick_axes()
+    lt_stick_btn = controller.left_stick_button()
+    rt_stick_btn = controller.right_stick_button()
+    hat = controller.hat()
+
+    # game logic
+
+
+    # drawing
+    screen.fill(BLACK)
+
+    ''' controller outline '''
+    pygame.draw.rect(screen, GREY, [40, 20, 520, 320], 3)
+    
+
+    ''' a, b, x, y '''
+    x, y = 450, 120
+    
+    if a_btn == 1:
+        pygame.draw.ellipse(screen, GREEN, [x + 30, y + 60, 25, 25])
+    else:
+        pygame.draw.ellipse(screen, GREEN, [x + 30, y + 60, 25, 25], 2)
+
+    if b_btn == 1:
+        pygame.draw.ellipse(screen, RED, [x + 60, y + 30, 25, 25])
+    else:
+        pygame.draw.ellipse(screen, RED, [x + 60, y + 30, 25, 25], 2)
+
+    if x_btn == 1:
+        pygame.draw.ellipse(screen, BLUE, [x, y + 30, 25, 25])
+    else:
+        pygame.draw.ellipse(screen, BLUE, [x, y + 30, 25, 25], 2)
+
+    if y_btn == 1:
+        pygame.draw.ellipse(screen, AMBER, [x + 30, y, 25, 25])
+    else:
+        pygame.draw.ellipse(screen, AMBER, [x + 30, y, 25, 25], 2)
+
+
+    ''' back, start '''
+    x, y = 250, 145
+    
+    if back == 1:
+        pygame.draw.ellipse(screen, WHITE, [x, y, 25, 20])
+    else:
+        pygame.draw.ellipse(screen, WHITE, [x, y, 25, 20], 2)
+
+    pygame.draw.ellipse(screen, GREY, [x + 40, y - 10, 40, 40])
+    
+    if start == 1:
+        pygame.draw.ellipse(screen, WHITE, [x + 95, y, 25, 20])
+    else:
+        pygame.draw.ellipse(screen, WHITE, [x + 95, y, 25, 20], 2)
+
+
+    ''' bumpers '''
+    x, y = 100, 50
+    
+    if lt_bump == 1:
+        pygame.draw.rect(screen, WHITE, [x, 50, y, 25])
+    else:
+        pygame.draw.rect(screen, WHITE, [x, 50, y, 25], 2)
+
+    if rt_bump == 1:
+        pygame.draw.rect(screen, WHITE, [x + 365, y, 50, 25])
+    else:
+        pygame.draw.rect(screen, WHITE, [x + 365, y, 50, 25], 2)
+
+    ''' triggers '''
+    x, y = 210, 60
+    
+    trigger_x = x + 100 + round(triggers * 100)
+    pygame.draw.line(screen, WHITE, [x, y], [x + 200, y])
+    pygame.draw.line(screen, WHITE, [trigger_x, y - 10], [trigger_x, y + 10])
+
+    ''' left stick '''
+    x, y = 65, 100
+    
+    left_x = x + 50 + round(lt_stick[0] * 50)
+    left_y = y + 50 + round(lt_stick[1] * 50)
+    
+    pygame.draw.line(screen, WHITE, [x + 60, y], [x + 60, y + 120], 1)
+    pygame.draw.line(screen, WHITE, [x, y + 60], [x + 120, y + 60], 1)
+    if lt_stick_btn == 0:
+        pygame.draw.ellipse(screen, WHITE, [left_x, left_y, 20, 20], 2)
+    else:
+        pygame.draw.ellipse(screen, WHITE, [left_x, left_y, 20, 20])
+
+    ''' right stick '''
+    x, y = 330, 190
+    
+    right_x = x + 50 + round(rt_stick[0] * 50)
+    right_y = y + 50 + round(rt_stick[1] * 50)
+    
+    pygame.draw.line(screen, WHITE, [x + 60, y], [x + 60, y + 120], 1)
+    pygame.draw.line(screen, WHITE, [x, y + 60], [x + 120, y + 60], 1)
+    if rt_stick_btn == 0:
+        pygame.draw.ellipse(screen, WHITE, [right_x, right_y, 20, 20], 2)
+    else:
+        pygame.draw.ellipse(screen, WHITE, [right_x, right_y, 20, 20])
+  
+    ''' hat '''
+    x, y = 180, 200
+    
+    pygame.draw.ellipse(screen, WHITE, [x, y, 100, 100])
+    if hat[0] == -1:
+        pygame.draw.ellipse(screen, GREY, [x, y + 40, 20, 20])
+    elif hat[0] == 1:
+        pygame.draw.ellipse(screen, GREY, [x + 80, y + 40, 20, 20])
+    if hat[1] == -1:
+        pygame.draw.ellipse(screen, GREY, [x + 40, y, 20, 20])
+    elif hat[1] == 1:
+        pygame.draw.ellipse(screen, GREY, [x + 40, y +80, 20, 20])
+
+
+    ''' joystick values '''
+    x, y = 50, 370
+    display_text(screen, "BUTTONS".format(a_btn), x, y)
+    display_text(screen, "A: {}".format(a_btn), x, y+ 25)
+    display_text(screen, "B: {}".format(b_btn), x, y + 50)
+    display_text(screen, "X: {}".format(x_btn), x, y + 75)
+    display_text(screen, "Y: {}".format(y_btn), x, y + 100)
+    display_text(screen, "LB: {}".format(lt_bump), x, y + 125)
+    display_text(screen, "RB: {}".format(rt_bump), x, y + 150)
+    display_text(screen, "Back: {}".format(back), x, y + 175)
+    display_text(screen, "Start: {}".format(start), x, y + 200)
+    display_text(screen, "LT Stick Btn: {}".format(lt_stick_btn), x, y + 225)
+    display_text(screen, "RT Stick Btn: {}".format(rt_stick_btn), x, y + 250)
+
+    display_text(screen, "AXES".format(a_btn), x + 275, y)
+    display_text(screen, "Left Stick: ({}, {})".format(round(lt_stick[0], 2), round(lt_stick[1], 2)), x + 275, y + 25)
+    display_text(screen, "Right Stick: ({}, {})".format(round(rt_stick[0], 2), round(rt_stick[1], 2)), x + 275, y + 50)
+    display_text(screen, "Triggers: {}".format(round(triggers, 2)), x + 275, y + 75)
+
+    display_text(screen, "HATS".format(a_btn), x + 275, y + 125)
+    display_text(screen, "Hat: {}".format(hat), x + 275, y + 150)
+
+    pygame.display.flip()
+
+
+    # limit refresh rate
+    clock.tick(FPS)
+    
+pygame.quit ()
---
+++ xbox_360_controller.py
@@ -0,0 +1,286 @@
+'''
+Unified XBox360 wired* controller interface for Python with Pygame.
+
+Versions:
+Python 3.4.3
+Pygame 1.9.2, SDL (1, 2, 15)
+
+Tested on:
+Mac OSX 10.9.5
+Ubuntu 15.04
+Windows 7
+
+Mac OSX does not have native controller support. The following driver was used. Mapping to another
+driver could be different.
+
+http://tattiebogle.net/index.php/ProjectRoot/Xbox360Controller/OsxDriver#toc1ss
+
+* On linux, only wired controllers use axes for the hat, wireless use buttons. Wireless support
+  not included yet.
+
+'''
+
+import pygame
+import sys
+
+LINUX = 0
+MAC = 1
+WINDOWS = 2
+
+class XBox360Controller:
+    
+    def __init__(self, num):
+        if sys.platform.startswith("lin"):
+            self.platform_id = LINUX
+        elif sys.platform.startswith("darwin"):
+            self.platform_id = MAC
+        elif sys.platform.startswith("win"):
+            self.platform_id = WINDOWS
+        
+        if self.platform_id == LINUX:
+            # buttons
+            self.A = 0
+            self.B = 1
+            self.X = 2
+            self.Y = 3
+            self.LEFT_BUMP = 4
+            self.RIGHT_BUMP = 5
+            self.BACK = 6
+            self.START = 7
+            self.LEFT_STICK_BTN = 9
+            self.RIGHT_STICK_BTN = 10
+
+            # axes
+            self.LEFT_STICK_X = 0
+            self.LEFT_STICK_Y = 1
+            self.RIGHT_STICK_X = 3
+            self.RIGHT_STICK_Y = 4
+            self.LEFT_TRIGGER = 2
+            self.RIGHT_TRIGGER = 5
+
+        elif self.platform_id == MAC:
+            # buttons
+            self.A = 11
+            self.B = 12
+            self.X = 13
+            self.Y = 14
+            self.LEFT_BUMP = 8
+            self.RIGHT_BUMP = 9
+            self.BACK = 5
+            self.START = 4
+            self.LEFT_STICK_BTN = 6
+            self.RIGHT_STICK_BTN = 7
+            self.HAT_UP = 0
+            self.HAT_DOWN = 1
+            self.HAT_LEFT = 2
+            self.HAT_RIGHT = 3	
+
+            # axes
+            self.LEFT_STICK_X = 0
+            self.LEFT_STICK_Y = 1
+            self.RIGHT_STICK_X = 2
+            self.RIGHT_STICK_Y = 3
+            self.LEFT_TRIGGER = 4
+            self.RIGHT_TRIGGER = 5
+    
+        elif self.platform_id == WINDOWS:
+            # buttons
+            self.A = 0
+            self.B = 1
+            self.X = 2
+            self.Y = 3
+            self.LEFT_BUMP = 4
+            self.RIGHT_BUMP = 5
+            self.BACK = 6
+            self.START = 7
+            self.LEFT_STICK_BTN = 8
+            self.RIGHT_STICK_BTN = 9
+
+            # axes
+            self.LEFT_STICK_X = 0
+            self.LEFT_STICK_Y = 1
+            self.RIGHT_STICK_X = 3
+            self.RIGHT_STICK_Y = 4
+            self.TRIGGERS = 2
+
+        self.joystick = pygame.joystick.Joystick(num)
+        self.joystick.init()
+        self.dead_zone = 0.15
+
+        self.left_trigger_used = False
+        self.right_trigger_used = False
+        
+    def dead_zone_adjust(self, value):
+        if value > self.dead_zone:
+            return (value - self.dead_zone) / (1 - self.dead_zone)
+        elif value < -self.dead_zone:
+            return (value + self.dead_zone) / (1 - self.dead_zone)
+        else:
+            return 0
+        
+    def a(self):
+        """
+        Returns 1 if A button is pressed, 0 otherwise.
+        """
+
+        return self.joystick.get_button(self.A)
+
+    def b(self):
+        """
+        Returns 1 if B button is pressed, 0 otherwise.
+        """
+
+        return self.joystick.get_button(self.B)
+
+    def x(self):
+        """
+        Returns 1 if X button is pressed, 0 otherwise.
+        """
+
+        return self.joystick.get_button(self.X)
+
+    def y(self):
+        """
+        Returns 1 if Y button is pressed, 0 otherwise.
+        """
+
+        return self.joystick.get_button(self.Y)
+    
+    def left_bumper(self):
+        """
+        Returns 1 if left bumper is pressed, 0 otherwise.
+        """
+
+        return self.joystick.get_button(self.LEFT_BUMP)
+
+    def right_bumper(self):
+        """
+        Returns 1 if right bumper is pressed, 0 otherwise.
+        """
+
+        return self.joystick.get_button(self.RIGHT_BUMP)
+
+    def back(self):
+        """
+        Returns 1 if Back button is pressed, 0 otherwise.
+        """
+
+        return self.joystick.get_button(self.BACK)
+
+    def start(self):
+        """
+        Returns 1 if Start button is pressed, 0 otherwise.
+        """
+
+        return self.joystick.get_button(self.START)
+
+    def left_stick_button(self):
+        """
+        Returns 1 if left stick button is pressed, 0 otherwise.
+        """
+
+        return self.joystick.get_button(self.LEFT_STICK_BTN)
+
+    def right_stick_button(self):
+        """
+        Returns 1 if right stick button is pressed, 0 otherwise.
+        """
+
+        return self.joystick.get_button(self.RIGHT_STICK_BTN)
+
+    def left_stick_axes(self):
+        """
+        Returns the x & y axes as a tuple such that
+
+            -1 <= x <= 1 && -1 <= y <= 1
+
+        Negative values are left and up.
+        Positive values are right and down.
+        """
+        
+        left_stick_x = self.dead_zone_adjust(self.joystick.get_axis(self.LEFT_STICK_X))
+        left_stick_y = self.dead_zone_adjust(self.joystick.get_axis(self.LEFT_STICK_Y))
+
+        return (left_stick_x, left_stick_y)
+
+    def right_stick_axes(self):
+        """
+        Returns the x & y axes as a tuple such that
+
+            -1 <= x <= 1 && -1 <= y <= 1
+
+        Negative values are left and up.
+        Positive values are right and down.
+        """
+
+        right_stick_x = self.dead_zone_adjust(self.joystick.get_axis(self.RIGHT_STICK_X))
+        right_stick_y = self.dead_zone_adjust(self.joystick.get_axis(self.RIGHT_STICK_Y))       
+            
+        return (right_stick_x, right_stick_y)
+
+    def triggers(self):
+        """
+        The triggers also form an axis. Full left trigger returns -1 and full right returns +1.
+        If the triggers are pulled simultaneously, then the sum of the trigger pulls is returned.
+
+        Notes:
+        On Windows, both triggers work additively to return a single axis, whereas triggers on
+        Linux and Mac function as independent axes. In this interface, triggers will behave
+        additively for all platforms so that pygame controllers will work consistently on each
+        platform.
+        
+        Also note that the value returned is on windows is multiplied by -1 so that negative is to
+        the left and positive to the right to be consistent with stick axes.
+
+        On Linux and Mac, trigger axes return 0 if they haven't been used yet. Once used, an unpulled
+        trigger returns 1 and pulled returns -1. The trigger_used booleans keep the math right for
+        triggers prior to use.
+        """
+
+        trigger_axis = 0.0
+        
+        if self.platform_id == LINUX or self.platform_id == MAC:
+            left = self.joystick.get_axis(self.LEFT_TRIGGER)
+            right = self.joystick.get_axis(self.RIGHT_TRIGGER)
+
+            if left != 0:
+                self.left_trigger_used = True
+            if right != 0:
+                self.right_trigger_used = True
+
+            if not self.left_trigger_used:
+                left = -1
+            if not self.right_trigger_used:
+                right = -1
+                
+            trigger_axis = (-1 * left + right) / 2
+            
+        elif self.platform_id == WINDOWS:
+            trigger_axis = -1 * self.joystick.get_axis(self.TRIGGERS)
+            
+        return trigger_axis
+
+
+    def hat(self):
+        """
+        The directional pad is a hat. The hat is returns an ordered pair as a tuple. Each value is
+        the set {-1, 0, 1}. The first value will be -1 if the hat is pressed on the left, and 1 if
+        pressed on the right. The second value will be -1 if the hat is pressed down and 1 if
+        pressed up.
+
+        Notes:
+        On Linux and Windows, the y value is multiplied by -1 to make it consistent with joystick
+        direction mapping.
+
+        The hat on a Mac is interpreted as 4 independent buttons with each returning either 0 or 1.
+        Button values are added to make tuple consistent with Linux/Windows behavior.
+        """
+
+        if self.platform_id == LINUX or self.platform_id == WINDOWS:
+            hat_x = self.joystick.get_hat(0)[0]
+            hat_y = -1 * self.joystick.get_hat(0)[1]
+        elif self.platform_id == MAC:
+            hat_x = -1 * self.joystick.get_button(self.HAT_LEFT) + self.joystick.get_button(self.HAT_RIGHT)
+            hat_y = -1 * self.joystick.get_button(self.HAT_UP) + self.joystick.get_button(self.HAT_DOWN)
+
+        return (hat_x, hat_y)
